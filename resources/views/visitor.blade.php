1491e40812071cb3198608eece981e6777a8ec2f
Harutoo <herconaryangga15@gmail.com>
2026-04-18 17:25:14 +0700

    Routing and Visitor frontend

---
+++ resources/views/visitor.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gym-In</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-black">
+    <!-- Header -->
+    <div class="sticky top-0 bg-black w-full">
+        <div class="flex items-center justify-between h-14 px-6">
+            <h1 class="text-2xl font-bold text-white">Gym-In</h1>
+            <div class="bg-white w-8 h-8 rounded-full"></div>
+        </div>
+    </div>
+    <!-- End Header -->
+
+    <div class="pt-[50vh] flex flex-col px-10 pb-20">
+        <div class="flex flex-col">
+            <h1 class="text-2xl font-bold text-white text-left">Gym-In</h1>
+            <p class="text-white text-left pt-3">
+                Tempat berolahraga dimana anda bisa memantau perkembangan anda, melihat kepadatan gym, serta memberi anda motivasi untuk berolahraga rutin dengan adanya fitur Streak langsung dari situs web aplikasi kami!
+            </p>
+        </div>
+
+        <button class="mt-8 bg-white text-black font-bold uppercase py-3 px-6 rounded-lg w-fit">
+            MARI BERGABUNG
+        </button>
+
+        <div class="text-white flex flex-col mt-8 justify-center items-center opacity-75">
+            <p class="font-semibold">Geser</p>
+            <p class="font-semibold">⭣</p>
+        </div>
+    </div>
+
+</body>
+</html>

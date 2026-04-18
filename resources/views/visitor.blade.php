908f757f1393e80f0a5b6acb17ac308b81038220
Harutoo <herconaryangga15@gmail.com>
2026-04-18 18:15:08 +0700

    Tinggal Background.

--- resources/views/visitor.blade.php
+++ resources/views/visitor.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body class="bg-black">
     <!-- Header -->
     <div class="sticky top-0 bg-black w-full">
@@ -18,11 +20,13 @@
     </div>
     <!-- End Header -->
 
+    <!-- Gym-In introduction text -->
     <div class="pt-[50vh] flex flex-col px-10 pb-20">
         <div class="flex flex-col">
-            <h1 class="text-2xl font-bold text-white text-left">Gym-In</h1>
+            <h1 class="text-2xl font-bold text-white text-left" style="text-shadow: 0px 5px 10px rgba(255, 255, 255, 0.4)">Gym-In</h1>
             <p class="text-white text-left pt-3">
-                Tempat berolahraga dimana anda bisa memantau perkembangan anda, melihat kepadatan gym, serta memberi anda motivasi untuk berolahraga rutin dengan adanya fitur Streak langsung dari situs web aplikasi kami!
+                Tempat berolahraga dimana anda bisa memantau perkembangan anda, melihat kepadatan gym, serta memberi
+                anda motivasi untuk berolahraga rutin dengan adanya fitur Streak langsung dari situs web aplikasi kami!
             </p>
         </div>
 
@@ -36,5 +40,89 @@
         </div>
     </div>
 
+    <!-- Streak and Reward showcase -->
+    <div class="flex flex-col justify-center items-center pt-16 px-10">
+        <div>
+            <h1 class="text-2xl text-white text-center font-bold" style="text-shadow: 0px 5px 10px rgba(255, 255, 255, 0.4)">
+                Tetap Semangat! <br>
+                Tetap Konsisten!
+            </h1>
+            <p class="text-white text-center pt-3 opacity-80">Semakin giat untuk berolahraga, semakin banyak hadiah yang
+                menunggumu!</p>
+        </div>
+
+        <div class="flex flex-row justify-between items-center pt-7">
+            {{-- <img src="" alt="" height="308px">
+            <img src="" alt="" height="308px"> --}}
+            <div style="height: 308px; background: #ffffff;"></div>
+            <div style="height: 308px; background: #ffffff;"></div>
+        </div>
+
+        <p class="text-white text-center pt-3 opacity-80">Tukarkan Poin Streak kamu dengan berbagai hadiah!</p>
+    </div>
+
+    <!-- Kepadatan Gym showcase -->
+    <div class="flex flex-col justify-center items-center pt-16 px-10">
+        <div>
+            <h1 class="text-2xl text-white text-left font-bold" style="text-shadow: 0px 5px 10px rgba(255, 255, 255, 0.4)">
+                Khawatir Penuh? <br>
+                Sumpek?
+            </h1>
+            <p class="text-white text-left pt-3 opacity-80">Cek kepadatan gym secara langsung di laman utama aplikasi!
+            </p>
+        </div>
+
+        <div class="flex flex-row justify-between items-center pt-7">
+            {{-- <img src="" alt="" height="308px">
+            <img src="" alt="" height="308px"> --}}
+            <div style="height: 308px; background: #ffffff;"></div>
+        </div>
+
+        <p class="text-white text-left pt-3 opacity-80">Kepadatan gym akan diperbarui setiap kali anggota datang dan
+            keluar dari gym!</p>
+    </div>
+
+    <!-- Kepadatan Gym showcase -->
+    <div class="flex flex-col justify-center items-end pt-16 px-10">
+        <div>
+            <h1 class="text-2xl text-white text-right font-bold" style="text-shadow: 0px 5px 10px rgba(255, 255, 255, 0.4)">
+                Pantau Perkembanganmu!
+            </h1>
+            <p class="text-white text-right pt-3 opacity-80">Catat dan lihat seberapa pesat perjalananmu!</p>
+        </div>
+
+        <div class="flex flex-row justify-between items-center pt-7">
+            {{-- <img src="" alt="" height="308px">
+            <img src="" alt="" height="308px"> --}}
+            <div style="height: 308px; background: #ffffff;"></div>
+        </div>
+
+        <p class="text-white text-right pt-3 opacity-80">Tunjukkanlah hasil kerja kerasmu yang sudah kamu peroleh!</p>
+    </div>
+
+    <!-- Tunggu apalagi ? -->
+    <div class="flex flex-col justify-center items-center py-16 px-10">
+        <div
+            class="flex flex-col justify-center items-center py-7 px-10 bg-white bg-opacity-20 backdrop-blur-3xl rounded-3xl">
+            <h1 class="text-2xl text-white text-center font-bold" style="text-shadow: 0px 5px 10px rgba(255, 255, 255, 0.4)">
+                Tunggu apalagi?
+            </h1>
+            <p class="text-white text-center pt-3 opacity-80">
+                Gabunglah bersama pejuang lainnya. <br>
+                Mulailah perjalananmu bersama
+            </p>
+
+            <h1 class="text-5xl text-white text-center font-bold py-10" style="text-shadow: 0px 0px 25px rgba(255, 255, 255, 0.4)">
+                Gym-In
+            </h1>
+
+            <button class="mt-8 bg-white text-black font-bold uppercase py-3 px-6 rounded-lg w-fit">
+                MARI BERGABUNG
+            </button>
+        </div>
+    </div>
+
+
 </body>
+
 </html>

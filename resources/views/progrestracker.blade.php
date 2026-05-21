5d4a6c727581b9310821534a6e05ee8113195f3e
Brian Serafino Donovan <briandonovan696@gmail.com>
2026-05-21 13:32:55 +0700

    Replace "Home" into "Beranda" in progress page

--- resources/views/progrestracker.blade.php
+++ resources/views/progrestracker.blade.php
@@ -292,7 +292,7 @@
                         d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
                     </path>
                 </svg>
-                <span class="text-xs mt-1">Home</span>
+                <span class="text-xs mt-1">Beranda</span>
             </a>
             <a href="{{ route('member.progres') }}" class="flex flex-col items-center text-emerald-400 transition">
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">

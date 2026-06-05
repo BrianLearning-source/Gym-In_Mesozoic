dbdc3415a0c595644d94c5425512a25970d5456f
Brian Serafino Donovan <briandonovan696@gmail.com>
2026-06-05 11:42:58 +0700

    Create New Anggota Form

--- app/Filament/Resources/AnggotaModels/Schemas/AnggotaModelForm.php
+++ app/Filament/Resources/AnggotaModels/Schemas/AnggotaModelForm.php
@@ -3,6 +3,10 @@
 namespace App\Filament\Resources\AnggotaModels\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\FileUpload;
 
 class AnggotaModelForm
 {
@@ -10,7 +14,61 @@ class AnggotaModelForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('username')
+                    ->label('Nama Pengguna')
+                    ->required()
+                    ->unique(ignoreRecord: true)
+                    ->maxLength(50),
+                TextInput::make('name')
+                    ->label('Nama Lengkap')
+                    ->required()
+                    ->maxLength(100),
+                TextInput::make('email')
+                    ->label('Email')
+                    ->required()
+                    ->email()
+                    ->unique(ignoreRecord: true)
+                    ->maxLength(100),
+                Select::make('gender')
+                    ->label('Jenis Kelamin')
+                    ->options([
+                        'Perempuan' => 'Perempuan',
+                        'Laki-laki' => 'Laki-laki',
+                    ]),
+                TextInput::make('phone_number')
+                    ->label('Nomor Telepon')
+                    ->tel()
+                    ->maxLength(20),
+                TextInput::make('password')
+                    ->label('Kata Sandi')
+                    ->password()
+                    ->hiddenOn('edit')
+                    ->dehydrated(fn ($state) => filled($state)),
+                TextInput::make('title')
+                    ->label('Gelar'),
+                DateTimePicker::make('join_date')
+                    ->label('Tanggal Bergabung')
+                    ->default(now()),
+                TextInput::make('points')
+                    ->label('Poin')
+                    ->numeric()
+                    ->default(0)
+                    ->minValue(0),
+                TextInput::make('streak')
+                    ->label('Streak')
+                    ->numeric()
+                    ->default(0)
+                    ->minValue(0),
+                TextInput::make('highest_streak')
+                    ->label('Streak Tertinggi')
+                    ->numeric()
+                    ->default(0)
+                    ->minValue(0),
+                TextInput::make('rest_days')
+                    ->label('Hari Istirahat')
+                    ->numeric()
+                    ->default(2)
+                    ->minValue(0),
             ]);
     }
 }

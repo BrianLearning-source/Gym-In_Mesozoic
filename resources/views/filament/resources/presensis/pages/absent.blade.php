b2528125684a40e49daae76c5817c35fba59b764
Brian Serafino Donovan <briandonovan696@gmail.com>
2026-06-14 21:17:35 +0700

    Change minor languange in absent page

--- resources/views/filament/resources/presensis/pages/absent.blade.php
+++ resources/views/filament/resources/presensis/pages/absent.blade.php
@@ -1,13 +1,13 @@
 <x-filament-panels::page>
     <div class="space-y-8">
         <div class="p-8 rounded-xl bg-white/5 border border-white/10">
-            <h2 class="text-xl font-semibold mb-6">Tambah Absensi</h2>
+            <h2 class="text-xl font-semibold mb-6">Tambah Presensi</h2>
             <form wire:submit="submit" class="space-y-6">
                 {{ $this->form }}
 
                 <div class="pt-4 flex justify-end">
                     <x-filament::button type="submit">
-                        Catat Absensi
+                        Catat Presensi
                     </x-filament::button>
                 </div>
             </form>
